406b2a0d6a28556094bb84d0742155796a619130
Hirokazu Miyaji <hirokazu.miyaji@gmail.com>
2019-07-08 22:29:43 +0900

    ts

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 const express = require('express')
+const cookiesMiddleware = require('universal-cookie-express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
@@ -7,12 +8,10 @@ const config = require('../nuxt.config.ts')
 config.dev = !(process.env.NODE_ENV === 'production')
 
 async function start() {
-  // Init Nuxt.js
   const nuxt = new Nuxt(config)
 
   const { host, port } = nuxt.options.server
 
-  // Build only in dev mode
   if (config.dev) {
     const builder = new Builder(nuxt)
     await builder.build()
@@ -20,7 +19,7 @@ async function start() {
     await nuxt.ready()
   }
 
-  // Give nuxt middleware to express
+  app.use(cookiesMiddleware())
   app.use(nuxt.render)
 
   // Listen the server
